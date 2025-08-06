178b14223bcd1a80f4035d37cac23fa50345c449
fudzich <vsevolod656@gmail.com>
2025-08-06 16:08:49 +0300

    Added level select to menu

---
+++ Team11_Project/Assets/Scripts/MenuUIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUIManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playButton;
+
+    [SerializeField]
+    GameObject tutorialLevelButton;
+
+    [SerializeField]
+    GameObject bigCityLevelButton;
+
+    [SerializeField]
+    GameObject retrunButton;
+
+    public void ToggleButtons()
+    {
+        playButton.SetActive(!playButton.activeSelf);
+        tutorialLevelButton.SetActive(!tutorialLevelButton.activeSelf);
+        bigCityLevelButton.SetActive(!bigCityLevelButton.activeSelf);
+        retrunButton.SetActive(!retrunButton.activeSelf);
+
+    }
+
+
+}
