9f273f0d8ea46f1001a10b81641d84353acc0516
unknown <jeremy2020fan@gmail.com>
2025-08-08 05:36:59 +0100

    add weapon animation

--- Team11_Project/Assets/Scripts/PlayerAnimationController.cs
+++ Team11_Project/Assets/Scripts/PlayerAnimationController.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerAnimationController : MonoBehaviour
 {
     private Animator animator;
-
+    public GameObject knife;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        if (knife != null)
+        {
+            knife.SetActive(false);
+        }
     }
 
     void Update()
@@ -23,6 +28,20 @@ public class PlayerAnimationController : MonoBehaviour
         if (Input.GetButtonDown("Fire2") || Input.GetButtonDown("Fire1"))
         {
             animator.SetTrigger("Attack");
+            if (knife != null && Input.GetButtonDown("Fire1"))
+            {
+                knife.SetActive(true);
+                StartCoroutine(DisableKnifeAfterDelay(0.5f)); //  disable after 0.5 seconds
+            }
+        }
+    }
+
+    private IEnumerator DisableKnifeAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (knife != null && knife.activeSelf)
+        {
+            knife.SetActive(false);
         }
     }
 }
