4e1960e90eef693ac4a6a0529c67a02d869df307
unknown <jeremy2020fan@gmail.com>
2025-08-13 00:27:16 +0100

    duplicated self-destory scripts by different names, remove them and merge them into projectiles

---
+++ Team11_Project/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModalManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject deathPanel;
+
+    [SerializeField]
+    GameObject winPanel;
+
+    public void ToggleDeathPanel()
+    {
+        deathPanel.SetActive(!deathPanel.activeSelf);
+    }
+
+    public void ToggleWinPanel()
+    {
+        winPanel.SetActive(!winPanel.activeSelf);
+    }
+}
