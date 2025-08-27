50f43b5495b5fe9dea589b53799312c9daace111
unknown <jeremy2020fan@gmail.com>
2025-08-27 05:21:10 +0100

    add heal assets: icon + practice

---
+++ Team11_Project/Assets/Scripts/Skills/Controllers/Heal.cs
@@ -0,0 +1,27 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Heal : MonoBehaviour, ISkill
+// {
+//     private bool isEnabled = false;
+//     private bool isOnCooldown;
+//     private float currentCooldown;
+
+//     private float maxCooldown;
+
+//     void Start()
+//     {
+//         isEnabled = true;
+//     }
+
+
+//     public bool IsInUse() => false;
+//     public void EnableSkill() => isEnabled = true;
+//     public float GetCurrentDuration() => 0f;
+//     public float GetMaxDuration() => 0f;
+//     public bool IsOnCooldown() => isOnCooldown;
+//     public float GetCurrentCooldown() => currentCooldown;
+//     public float GetMaxCooldown() => maxCooldown;
+
+// }
