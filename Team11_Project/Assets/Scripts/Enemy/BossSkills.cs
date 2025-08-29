f5001d5a93810ec1a08b5de6733244e40ba22d6b
unknown <jeremy2020fan@gmail.com>
2025-08-29 03:57:58 +0100

    improve skill: add range on spinning

--- Team11_Project/Assets/Scripts/Enemy/BossSkills.cs
+++ Team11_Project/Assets/Scripts/Enemy/BossSkills.cs
@@ -6,12 +6,19 @@ public class BossSkills : MonoBehaviour
 {
     private Animator animator;
     private NavMeshAgent agent;
-    [SerializeField] private GameObject sandParticle;
+    [SerializeField] private GameObject sandParticlePrefab;
+
+    private GameObject sandFX;
+    private ParticleSystem sandPS;
     public bool isUsingSkill { get; private set; }
     private float lastSkillTime;
     [SerializeField] private float skillCooldown = 5f;
     [SerializeField] private float skillTriggerProbability = 0.5f;
     [SerializeField] private ParticleSystem fireParticle;
+
+    private CapsuleCollider capsule;
+    [SerializeField] private float normalRadius = 0.5f;
+    [SerializeField] private float skillRadius = 1.2f;
 
     private float defaultDamage;
     void Start()
@@ -19,6 +26,17 @@ public class BossSkills : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
         defaultDamage = GetComponent<EnemyStatus>().getDamage();
+        capsule = GetComponent<CapsuleCollider>();
+
+        if (sandParticlePrefab != null)
+        {
+            sandFX = Instantiate(sandParticlePrefab, transform);
+            sandFX.transform.localPosition = Vector3.zero;
+            sandFX.transform.localRotation = Quaternion.Euler(0f, -90f, 0f);
+            sandPS = sandFX.GetComponent<ParticleSystem>();
+            sandFX.SetActive(false);
+        }
+
     }
 
     void Update()
@@ -58,20 +76,24 @@ public class BossSkills : MonoBehaviour
         isUsingSkill = true;
         animator.SetBool("IsRolling", true);
         agent.isStopped = true;
-        sandParticle.transform.position = transform.position;
-        sandParticle.transform.rotation = transform.rotation * Quaternion.Euler(0f, -90f, 0f);
-        sandParticle.SetActive(true);
+        // show sand
+        if (sandFX)
+        {
+            sandFX.SetActive(true);
+            if (sandPS) { sandPS.Clear(true); sandPS.Play(true); }
+        }
         GetComponent<EnemyStatus>().setDamage(defaultDamage * 2);
 
         // reset the states
-        yield return new WaitForSeconds(0.5f); // [TODO: hardcoding, fix later]
+        yield return new WaitForSeconds(0.5f);
         animator.SetBool("IsRolling", false);
         agent.isStopped = false;
         isUsingSkill = false;
         GetComponent<EnemyStatus>().setDamage(defaultDamage);
 
-        yield return new WaitForSeconds(0.5f); // [TODO: hardcoding, fix later]
-        sandParticle.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        if (sandPS) sandPS.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        if (sandFX) sandFX.SetActive(false);
     }
     private void TriggerSpin()
     {
@@ -83,15 +105,27 @@ public class BossSkills : MonoBehaviour
         isUsingSkill = true;
         animator.SetBool("IsSpinning", true);
         agent.isStopped = true;
-        fireParticle.Play();
+        fireParticle.Clear(true);
+        fireParticle.Play(true);
+        SetCapsuleCollider(skillRadius);
 
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(0.5f);
 
         animator.SetBool("IsSpinning", false);
         agent.isStopped = false;
-        fireParticle.Stop();
         isUsingSkill = false;
 
+        yield return new WaitForSeconds(4f);
+        // fireParticle.Stop();
+        fireParticle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        SetCapsuleCollider(normalRadius);
+
     }
+
+    private void SetCapsuleCollider(float radius)
+    {
+        capsule.radius = radius;
+    }
+
 
 }
